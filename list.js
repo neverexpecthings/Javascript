e5810c788afc0f257305a58126eaf84abf79241d
neverexpecthings <neverexpecthings@gmail.com>
2021-06-26 21:02:37 -0400

    Last Update

--- list.js
+++ list.js
@@ -1,14 +1,23 @@
 let linkedList = {
-value: 1,
-pointer: {
-value: 2,
-pointer: {
-value: 3,
-pointer: null
-}
-}
+  value: 1,
+  pointer: {
+    value: 2,
+    pointer: {
+      value: 3,
+      pointer: null,
+    },
+  },
+};
+let arr = [1, 2, 3];
+let arrayToList = function (arr) {
+  let list = null;
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const element = arr[i];
+    list = { value: element, pointer: list };
+  }
+
+  console.log(list);
 };
 
-let arrayToList = function(arr) {
-  
-}
+arrayToList(arr);
