cefcc8d79053264102faae11eed73f7a002178e7
neverexpecthings <neverexpecthings@gmail.com>
2021-06-28 18:24:21 -0400

    Man i love data structures

--- list.js
+++ list.js
@@ -9,15 +9,29 @@ let linkedList = {
   },
 };
 let arr = [1, 2, 3];
+
 let arrayToList = function (arr) {
   let list = null;
-
+  //! Here we iterate backward the array
   for (let i = arr.length - 1; i >= 0; i--) {
     const element = arr[i];
+    //* Here we add the index to value then pointer will add a new node
     list = { value: element, pointer: list };
   }
 
   console.log(list);
 };
 
+let listToArray = function (list) {
+  let array = [];
+  //! What other way can be use?
+  for (let node = list; node; node = node.pointer) {
+    const element = node.value;
+    array.push(element);
+  }
+  console.log(array);
+};
+
 arrayToList(arr);
+
+listToArray(linkedList);
